485b1c87a720055b68a63d2b64e9256a41aeeacb
shabazdxb <shabazdxb8@gmail.com>
2026-02-26 14:21:41 +0400

    Update broken_travel.py
    edited by M01087871

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,14 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
-
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
-    print ("That's totally the present!")
-elif:
-    print ("Far out, that's the future!!")
+year = int(input("Greetings! What is your year of origin? "))#Changed from '==' to '='
+#changed input syntax. instead of"int.input(...." to "int(input(...."
+#changed quotations from ' to "
+if year < 1900:#strictly following logic insted of using "<="
+    #added missing colon
+    print("Woah, that's the past!")
+    #changed to double quotations as python gives error with single
+elif year >= 1900 and year <= 2020:#changed wrong logic operator
+    print("That's totally the present!")
+else:#changed from elif and put else
+    print("Far out, that's the future!!")
